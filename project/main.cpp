b5025497f1b625c6328f6660998a9594d176a7a2
Michael Garcia <micjosgar@gmail.com>
2021-10-14 22:09:38 -0400

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,11 @@
+/*
+    Project 1 - CSCI 373
+    David Slaby and Michael Garcia
+    Professor Ni Lu
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!\n";
+    return 0;
+}
