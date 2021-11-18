0f3087a563c0ae0f3787d2ba9c669add304398dd
Michael Garcia <micjosgar@gmail.com>
2021-11-18 02:33:01 -0500

    Revision almost final

--- project/main.cpp
+++ project/main.cpp
@@ -6,100 +6,44 @@
     Merging two 
  */
 #include <iostream>
-#include <assert.h>
 using namespace std;
 
 /* Link list node */
-class Node
-{
-	public:
-	int data;
-	Node* next;
+class ListNode {
+public:
+	int val;
+	ListNode *next;
 };
 
-/* pull off the front node of
-the source and put it in dest */
-void MoveNode(Node** destRef, Node** sourceRef);
-
-/* Takes two lists sorted in increasing
-order, and splices their nodes together
-to make one big sorted list which
-is returned. */
-Node* SortedMerge(Node* a, Node* b)
-{
-	/* a dummy first node to hang the result on */
-	Node dummy;
-
-	/* tail points to the last result node */
-	Node* tail = &dummy;
-
-	/* so tail->next is the place to
-	add new nodes to the result. */
-	dummy.next = NULL;
-	while (1)
-	{
-		if (a == NULL)
-		{
-			/* if either list runs out, use the
-			other list */
-			tail->next = b;
-			break;
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+	ListNode fake;						// Create a dummy node
+	ListNode *last = &fake;				// Last points to the last result node
+	while (l1 != NULL && l2 != NULL) {
+		if (l1->val < l2->val) {
+			last->next = l1;
+			last = l1;
+			l1 = l1->next;
+		} else {
+			last->next = l2;
+			last = l2;
+			l2 = l2->next;
 		}
-		else if (b == NULL)
-		{
-			tail->next = a;
-			break;
-		}
-		if (a->data <= b->data)
-			MoveNode(&(tail->next), &a);
-		else
-			MoveNode(&(tail->next), &b);
-
-		tail = tail->next;
 	}
-	return(dummy.next);
+	if (l1 != NULL) {
+		last->next = l1;
+	}
+	if (l2 != NULL) {
+		last->next = l2;
+	}
+	return fake.next;
 }
 
-/* UTILITY FUNCTIONS */
-/* MoveNode() function takes the
-node from the front of the source,
-and move it to the front of the dest.
-It is an error to call this with the
-source list empty.
-
-Before calling MoveNode():
-source == {1, 2, 3}
-dest == {1, 2, 3}
-
-After calling MoveNode():
-source == {2, 3}
-dest == {1, 1, 2, 3} */
-void MoveNode(Node** destRef, Node** sourceRef)
-{
-	/* the front source node */
-	Node* newNode = *sourceRef;
-	assert(newNode != NULL);
-
-	/* Advance the source pointer */
-	*sourceRef = newNode->next;
-
-	/* Link the old dest off the new node */
-	newNode->next = *destRef;
-
-	/* Move dest to point to the new node */
-	*destRef = newNode;
-}
-
-
-/* Function to insert a node at
-the beginning of the linked list */
-void push(Node** head_ref, int new_data)
-{
+void push(ListNode **head_ref, int new_data) {
 	/* allocate node */
-	Node* new_node = new Node();
+	ListNode *new_node = new ListNode();
 
 	/* put in the data */
-	new_node->data = new_data;
+	new_node->val = new_data;
 
 	/* link the old list off the new node */
 	new_node->next = (*head_ref);
@@ -108,39 +52,30 @@ void push(Node** head_ref, int new_data)
 	(*head_ref) = new_node;
 }
 
-/* Function to print nodes in a given linked list */
-void printList(Node *node)
-{
-    while (node != NULL)
-    {
-        cout << node->data << " ";
-        node = node->next;
-    }
+void printList(ListNode *node) {
+	while (node != NULL) {
+		cout << node->val << " ";
+		node = node->next;
+	}
 }
 
-int main()
-{
-	/* Start with the empty list */
-	Node* res = NULL;
-	Node* a = NULL;
-	Node* b = NULL;
+int main() {
+	ListNode *l1 = NULL;
+	ListNode *l2 = NULL;
+	ListNode *result = NULL;
 
-	/* Let us create two sorted linked lists
-	to test the functions
-	Created lists, a: 5->10->15, b: 2->3->20 */
-	push(&a, 15);
-	push(&a, 10);
-	push(&a, 5);
+	push(&l1, 15);
+	push(&l1, 10);
+	push(&l1, 5);
 
-	push(&b, 20);
-	push(&b, 3);
-	push(&b, 2);
+	push(&l2, 20);
+	push(&l2, 3);
+	push(&l2, 2);
 
-	/* Remove duplicates from linked list */
-	res = SortedMerge(a, b);
+	result = mergeTwoLists(l1, l2);
 
 	cout << "Merged Linked List is: \n";
-	printList(res);
+	printList(result);
 
 	return 0;
 }
